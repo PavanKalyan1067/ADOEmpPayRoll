4211c04124f4118eb620d65efffcfc77066d51cb
Pavan Kalyan <pavankalyanyadav321@gmail.com>
2022-04-24 17:57:57 +0530

    Welcome to ADO Emp Pay Roll

---
+++ ADOEmployeePayRoll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADOEmployeePayRoll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ADO Employee Pay Roll");
+        }
+    }
+}
