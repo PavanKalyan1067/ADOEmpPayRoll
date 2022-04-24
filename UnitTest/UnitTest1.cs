bb28b7160a95c84e63117b437c25f521ffcb8127
Pavan Kalyan <pavankalyanyadav321@gmail.com>
2022-04-24 18:23:20 +0530

    RetrieveBasedonName

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -37,5 +37,6 @@ namespace ADOEmployeePayrollTesting
             int actual = employeeRepository.UpdateSalary(employeeDataManager);
             Assert.AreEqual(actual, expected);
         }
+
     }
 }
